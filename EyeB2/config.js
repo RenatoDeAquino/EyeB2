0acb827a9fd0b9748ea11f8f1455d666d738ea24
RenatoDeAquino <43344209+RenatoDeAquino@users.noreply.github.com>
2019-04-12 08:39:09 -0300

    Delete config.js
    informações pessoais do banco de dados

--- EyeB2/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC0DtAnB4d4B_dRFvwVVMvAOsyBU44HHZY",
-    authDomain: "eyeb-2fc1a.firebaseapp.com",
-    databaseURL: "https://eyeb-2fc1a.firebaseio.com",
-    projectId: "eyeb-2fc1a",
-    storageBucket: "eyeb-2fc1a.appspot.com",
-    messagingSenderId: "563681818220"
-  };
